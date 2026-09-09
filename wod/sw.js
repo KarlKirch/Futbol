9620b4c9325792c058e9a58c681f66fc73a0b9c7
KarlKirch <kirch.karl@gmail.com>
2026-09-09 21:25:01 +0300

    Refresh CrossFit service worker cache after freeze fix

--- wod/sw.js
+++ wod/sw.js
@@ -1,4 +1,4 @@
-const CACHE='crossfit-v1';
+const CACHE='crossfit-v2';
 const ASSETS=['./app.html','./index.html','./crossfit.webmanifest','./crossfit-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>(k.startsWith('wod-')||k.startsWith('crossfit-'))&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
